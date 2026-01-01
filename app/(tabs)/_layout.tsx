2eb3d656f6f4d5a59aa6b7c658cba3c4becde37a
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-01 23:48:00 +0530

    Add tab bar icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,52 @@
+import { Ionicons, Octicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 import React from "react";
 
 const TabsLayout = () => {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="HomeScreen" options={{ title: "Home" }} />
-      <Tabs.Screen name="RoomsScreen" options={{ title: "Rooms" }} />
+    <Tabs
+      screenOptions={{ headerShown: false, tabBarActiveTintColor: "#ff6900" }}
+    >
+      <Tabs.Screen
+        name="HomeScreen"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ size, color, focused }) => (
+            <Octicons
+              name={focused ? "home-fill" : "home"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="RoomsScreen"
+        options={{
+          title: "Rooms",
+          tabBarIcon: ({ size, color, focused }) => (
+            <Ionicons
+              name={
+                focused ? "chatbubble-ellipses" : "chatbubble-ellipses-outline"
+              }
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
       <Tabs.Screen
         name="LeaderboardScreen"
-        options={{ title: "Leaderboard" }}
+        options={{
+          title: "Leaderboard",
+          tabBarIcon: ({ size, color, focused }) => (
+            <Ionicons
+              name={focused ? "trophy" : "trophy-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
       />
     </Tabs>
   );
