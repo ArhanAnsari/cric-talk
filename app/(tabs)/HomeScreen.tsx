e24264ea9eb3908c7332d9bd4833b23d7a4ca512
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-02 16:28:40 +0530

    Add create post button and modal UI in HomeScreen

--- app/(tabs)/HomeScreen.tsx
+++ app/(tabs)/HomeScreen.tsx
@@ -1,10 +1,13 @@
-import { Octicons } from "@expo/vector-icons";
+import { Ionicons, Octicons } from "@expo/vector-icons";
 import React, { useState } from "react";
-import { Pressable, Text, TextInput, View } from "react-native";
+import { Modal, Pressable, Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
   const [searchQuery, setSearchQuery] = useState<string>("");
+  const [isVisible, setIsVisible] = useState<boolean>(true);
+
+  const [content, setContent] = useState<string>("");
 
   return (
     <View className="flex-1 bg-white">
@@ -92,6 +95,55 @@ const HomeScreen = () => {
           </View>
         </View>
       </View>
+
+      {/* CREATE POST BUTTON */}
+      <Pressable className="w-16 h-16 bg-orange-500 rounded-full items-center justify-center absolute bottom-6 right-6 shadow-md elevation-xs">
+        <Octicons name="plus" size={24} color="white" />
+      </Pressable>
+
+      {/* CREATE POST MODAL */}
+      <Modal visible={true} transparent animationType="slide">
+        <View className="flex-1 bg-white">
+          <SafeAreaView className="flex-1">
+            {/* MODAL HEADER */}
+            <View className="flex-row items-center justify-between px-6 py-4 border-b border-gray-200">
+              <Ionicons
+                name="close"
+                size={24}
+                color="#0f172b"
+                onPress={() => setIsVisible(false)}
+              />
+
+              <Pressable>
+                <Text className="text-orange-500 font-semibold">
+                  Create Post
+                </Text>
+              </Pressable>
+            </View>
+
+            {/* MODAL CONTENT */}
+            <View className="px-6 py-4">
+              <Text className="text-lg font-semibold text-slate-900 tracking-wide">
+                What's on your mind?
+              </Text>
+
+              <TextInput
+                value={content}
+                onChangeText={setContent}
+                placeholder="Write anything about cricket..."
+                multiline
+                numberOfLines={8}
+                maxLength={512}
+                className="border border-gray-300 mt-2 rounded-lg text-slate-900"
+              />
+
+              <Text className="text-sm text-gray-800 mt-2 ml-auto">
+                {content.length} / 512
+              </Text>
+            </View>
+          </SafeAreaView>
+        </View>
+      </Modal>
     </View>
   );
 };
