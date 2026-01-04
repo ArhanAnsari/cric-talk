469c57250875d4c460c92addcd56c2ea6287b6ba
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-04 08:46:22 +0530

    Refactor PostDetails component layout with KeyboardAvoidingView and improved comment section structure

--- app/(posts)/[postId].tsx
+++ app/(posts)/[postId].tsx
@@ -4,7 +4,14 @@ import { usePosts } from "@/store/usePosts";
 import { Ionicons, Octicons } from "@expo/vector-icons";
 import { router, useLocalSearchParams } from "expo-router";
 import React, { useEffect, useState } from "react";
-import { Pressable, ScrollView, Text, TextInput, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Pressable,
+  ScrollView,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const PostDetails = () => {
@@ -51,151 +58,165 @@ const PostDetails = () => {
   }
 
   return (
-    <View className="flex-1 bg-white">
-      {/* HEADER */}
-      <View className="w-full h-30 bg-orange-500">
-        <SafeAreaView>
-          <View className="px-6 flex-row items-center w-full">
-            <Octicons
-              name="arrow-left"
-              size={24}
-              color="white"
-              onPress={() => router.back()}
-            />
+    <KeyboardAvoidingView
+      className="flex-1 bg-white"
+      behavior="padding"
+      keyboardVerticalOffset={0}
+    >
+      <View className="flex-1">
+        {/* HEADER */}
+        <View className="w-full h-30 bg-orange-500">
+          <SafeAreaView>
+            <View className="px-6 flex-row items-center w-full">
+              <Octicons
+                name="arrow-left"
+                size={24}
+                color="white"
+                onPress={() => router.back()}
+              />
 
-            <Text className="text-white font-semibold text-xl text-center flex-1">
-              CricTalk
-            </Text>
-          </View>
-        </SafeAreaView>
-      </View>
-
-      <ScrollView
-        className="flex-1"
-        contentContainerStyle={{ paddingBottom: 60 }}
-        showsVerticalScrollIndicator={false}
-      >
-        <SafeAreaView>
-          {/* POST DETAILS */}
-          <View className="px-6 -mt-4">
-            {/* POST CONTENT */}
-            <View className="border-b border-gray-300 pb-4">
-              {/* AUTHOR INFO + PUBLISH DATE */}
-              <View className="flex-row items-center gap-2">
-                {/* AUTHOR PROFILE IMAGE */}
-                <Pressable className="bg-gray-300 h-10 w-10 items-center justify-center rounded-full">
-                  <Text className="text-lg font-semibold capitalize text-slate-950">
-                    {post?.authorId[0]}
-                  </Text>
-                </Pressable>
-
-                {/* AUTHOR NAME */}
-                <Text className="text-lg font-medium text-slate-900">
-                  {post?.authorId}
-                </Text>
-
-                {/* PUBLISH DATE */}
-                <Text className="text-sm text-slate-600 ml-auto">
-                  {new Date(post?.$createdAt || "").toLocaleDateString(
-                    "en-IN",
-                    {
-                      year: "numeric",
-                      month: "short",
-                      day: "numeric",
-                      hour: "numeric",
-                      minute: "numeric",
-                    }
-                  )}
-                </Text>
-              </View>
-
-              {/* POST CONTENT */}
-              <View className="mt-4">
-                <Text className="leading-6 text-slate-800">
-                  {post?.content}
-                </Text>
-              </View>
-
-              {/* IMAGE PLACEHOLDER */}
-              {post?.image?.length !== 0 && (
-                <View className="w-full aspect-video bg-gray-300 rounded-lg my-4" />
-              )}
-
-              {/* POST ACTIONS */}
-              <View className="mt-4 flex-row items-center justify-between">
-                <Pressable className="flex-row gap-2" onPress={handleLikePost}>
-                  <Octicons
-                    name={
-                      post?.likedBy.includes(userId) ? "heart-fill" : "heart"
-                    }
-                    size={18}
-                    color={post?.likedBy.includes(userId) ? "red" : "gray"}
-                  />
-                  <Text>
-                    {post?.likes} Like{post?.likes === 1 ? "" : "s"}
-                  </Text>
-                </Pressable>
-
-                <Pressable className="flex-row gap-2">
-                  <Octicons name="comment-discussion" size={18} color="black" />
-                  <Text>
-                    {post?.comments.length} Comment
-                    {post?.comments.length === 1 ? "" : "s"}
-                  </Text>
-                </Pressable>
-
-                <Pressable className="flex-row gap-2">
-                  <Octicons name="eye" size={18} color="black" />
-                  <Text>
-                    {post?.views} View{post?.views === 1 ? "" : "s"}
-                  </Text>
-                </Pressable>
-              </View>
-            </View>
-
-            {/* COMMENTS SECTION */}
-            <View className="mt-4">
-              <Text className="text-slate-900 font-semibold text-xl">
-                Comments
+              <Text className="text-white font-semibold text-xl text-center flex-1">
+                CricTalk
               </Text>
+            </View>
+          </SafeAreaView>
+        </View>
 
-              {/* COMMENT LIST */}
-              <View className="border-b border-gray-300 pb-4 mt-4">
+        <ScrollView
+          className="flex-1"
+          contentContainerStyle={{ paddingBottom: 60 }}
+          showsVerticalScrollIndicator={false}
+        >
+          <SafeAreaView>
+            {/* POST DETAILS */}
+            <View className="px-6 -mt-4">
+              {/* POST CONTENT */}
+              <View className="border-b border-gray-300 pb-4">
                 {/* AUTHOR INFO + PUBLISH DATE */}
                 <View className="flex-row items-center gap-2">
                   {/* AUTHOR PROFILE IMAGE */}
                   <Pressable className="bg-gray-300 h-10 w-10 items-center justify-center rounded-full">
                     <Text className="text-lg font-semibold capitalize text-slate-950">
-                      i
+                      {post?.authorId[0]}
                     </Text>
                   </Pressable>
 
                   {/* AUTHOR NAME */}
                   <Text className="text-lg font-medium text-slate-900">
-                    Ishan
+                    {post?.authorId}
                   </Text>
 
                   {/* PUBLISH DATE */}
                   <Text className="text-sm text-slate-600 ml-auto">
-                    3 Jan 2025, 08:25 AM
+                    {new Date(post?.$createdAt || "").toLocaleDateString(
+                      "en-IN",
+                      {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                        hour: "numeric",
+                        minute: "numeric",
+                      }
+                    )}
                   </Text>
                 </View>
 
-                {/* COMMENT CONTENT */}
+                {/* POST CONTENT */}
                 <View className="mt-4">
                   <Text className="leading-6 text-slate-800">
-                    This will contain the comment content for the clicked post.
+                    {post?.content}
                   </Text>
+                </View>
+
+                {/* IMAGE PLACEHOLDER */}
+                {post?.image?.length !== 0 && (
+                  <View className="w-full aspect-video bg-gray-300 rounded-lg my-4" />
+                )}
+
+                {/* POST ACTIONS */}
+                <View className="mt-4 flex-row items-center justify-between">
+                  <Pressable
+                    className="flex-row gap-2"
+                    onPress={handleLikePost}
+                  >
+                    <Octicons
+                      name={
+                        post?.likedBy.includes(userId) ? "heart-fill" : "heart"
+                      }
+                      size={18}
+                      color={post?.likedBy.includes(userId) ? "red" : "gray"}
+                    />
+                    <Text>
+                      {post?.likes} Like{post?.likes === 1 ? "" : "s"}
+                    </Text>
+                  </Pressable>
+
+                  <Pressable className="flex-row gap-2">
+                    <Octicons
+                      name="comment-discussion"
+                      size={18}
+                      color="black"
+                    />
+                    <Text>
+                      {post?.comments.length} Comment
+                      {post?.comments.length === 1 ? "" : "s"}
+                    </Text>
+                  </Pressable>
+
+                  <Pressable className="flex-row gap-2">
+                    <Octicons name="eye" size={18} color="black" />
+                    <Text>
+                      {post?.views} View{post?.views === 1 ? "" : "s"}
+                    </Text>
+                  </Pressable>
+                </View>
+              </View>
+
+              {/* COMMENTS SECTION */}
+              <View className="mt-4">
+                <Text className="text-slate-900 font-semibold text-xl">
+                  Comments
+                </Text>
+
+                {/* COMMENT LIST */}
+                <View className="border-b border-gray-300 pb-4 mt-4">
+                  {/* AUTHOR INFO + PUBLISH DATE */}
+                  <View className="flex-row items-center gap-2">
+                    {/* AUTHOR PROFILE IMAGE */}
+                    <Pressable className="bg-gray-300 h-10 w-10 items-center justify-center rounded-full">
+                      <Text className="text-lg font-semibold capitalize text-slate-950">
+                        i
+                      </Text>
+                    </Pressable>
+
+                    {/* AUTHOR NAME */}
+                    <Text className="text-lg font-medium text-slate-900">
+                      Ishan
+                    </Text>
+
+                    {/* PUBLISH DATE */}
+                    <Text className="text-sm text-slate-600 ml-auto">
+                      3 Jan 2025, 08:25 AM
+                    </Text>
+                  </View>
+
+                  {/* COMMENT CONTENT */}
+                  <View className="mt-4">
+                    <Text className="leading-6 text-slate-800">
+                      This will contain the comment content for the clicked
+                      post.
+                    </Text>
+                  </View>
                 </View>
               </View>
             </View>
-          </View>
-        </SafeAreaView>
-      </ScrollView>
+          </SafeAreaView>
+        </ScrollView>
+      </View>
 
       {/* COMMENT INPUT BOX */}
       <SafeAreaView>
-        <View className="px-6 flex-row items-center">
+        <View className="px-6 flex-row items-center ">
           {/* COMMENT INPUT */}
           <TextInput
             placeholder="Comment"
@@ -208,7 +229,7 @@ const PostDetails = () => {
           </Pressable>
         </View>
       </SafeAreaView>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
