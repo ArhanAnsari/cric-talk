deaace113baf98c3d617a46efcb6100fe807aca2
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-05 22:41:13 +0530

    Add room data fetching in RoomDiscussion component

--- app/(rooms)/[roomId].tsx
+++ app/(rooms)/[roomId].tsx
@@ -1,9 +1,30 @@
+import { Room } from "@/interfaces/Room";
+import { fetchRooms } from "@/services/rooms.service";
 import { useLocalSearchParams } from "expo-router";
-import React from "react";
+import React, { useEffect } from "react";
 import { Text, View } from "react-native";
 
 const RoomDiscussion = () => {
   const { roomId } = useLocalSearchParams();
+
+  const [room, setRoom] = React.useState<Room | null>(null);
+
+  useEffect(() => {
+    let mounted = true;
+
+    async function loadRoomDetails() {
+      if (!mounted) return;
+
+      const data = await fetchRooms();
+      const roomDetails = data.rows.find((room) => room.$id === roomId);
+      setRoom(roomDetails || null);
+    }
+    loadRoomDetails();
+
+    return () => {
+      mounted = false;
+    };
+  }, []);
 
   return (
     <View>
