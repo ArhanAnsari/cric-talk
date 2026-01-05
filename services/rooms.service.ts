90e2b15e932cf50fd398154f57ab89ff2bfe825f
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-05 07:20:11 +0530

    Add createRoom and deleteRoom functions in rooms service

--- services/rooms.service.ts
+++ services/rooms.service.ts
@@ -1,5 +1,5 @@
 import { tablesDB } from "@/libs/appwrite";
-import { Query } from "react-native-appwrite";
+import { ID, Query } from "react-native-appwrite";
 
 const CRIC_TALK_DATABASE_ID =
   process.env.EXPO_PUBLIC_APPWRITE_CRIC_TALK_DATABASE_ID!;
@@ -15,5 +15,42 @@ export async function fetchRooms() {
   } catch (error) {
     console.log(`Error while fetching rhe rooms ${error}`);
     throw error;
+  }
+}
+
+export async function createRoom(
+  teams: string[],
+  status: "upcoming" | "live" | "finished",
+  startTime: string,
+  matchType: "ODI" | "TEST" | "T20",
+  endTime?: string
+) {
+  try {
+    return await tablesDB.createRow({
+      databaseId: CRIC_TALK_DATABASE_ID,
+      tableId: ROOMS_TABLE_ID,
+      rowId: ID.unique(),
+      data: {
+        teams,
+        status,
+        startTime,
+        endTime,
+        matchType,
+      },
+    });
+  } catch (error) {
+    console.log(`Error while creating room ${error}`);
+  }
+}
+
+export async function deleteRoom(roomId: string) {
+  try {
+    await tablesDB.deleteRow({
+      databaseId: CRIC_TALK_DATABASE_ID,
+      tableId: ROOMS_TABLE_ID,
+      rowId: roomId,
+    });
+  } catch (error) {
+    console.log(`Error while deleting the room ${error}`);
   }
 }
