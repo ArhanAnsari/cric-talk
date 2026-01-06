059a09045cdd01555987242b2d8c6ed1645c7a22
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-06 07:04:51 +0530

    Add authorName and isEdited to RoomMessage interface

--- interfaces/RoomMessage.ts
+++ interfaces/RoomMessage.ts
@@ -2,6 +2,11 @@ import { Models } from "react-native-appwrite";
 
 export interface RoomMessage extends Models.Row {
   roomId: string;
+
   authorId: string;
+  authorName: string;
+
   content: string;
+
+  isEdited: boolean;
 }
