d4ec6d7f31ceee609e455fa5282d421527a2c25d
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-07 21:45:43 +0530

    Add create, update and delete room message functions in room-message-guard function

--- functions/room-message-guard/index.ts
+++ functions/room-message-guard/index.ts
@@ -1,4 +1,4 @@
-import { Client, TablesDB, Users } from "node-appwrite";
+import { Client, ID, TablesDB, Users } from "node-appwrite";
 
 export default async ({ req, res }) => {
   const { roomId, content } = req.body;
@@ -8,6 +8,72 @@ export default async ({ req, res }) => {
     .setProject(process.env.APPWRITE_PROJECT_ID!)
     .setKey(process.env.APPWRIT_API_KEY!);
 
+  const CRIC_TALK_DATABASE_ID = process.env.CRIC_TALK_DATABASE_ID!;
+  const ROOMS_TABLE_ID = process.env.ROOMS_TABLE_ID!;
+  const ROOM_MESSAGE_TABLE_ID = process.env.ROOM_MESSAGE_TABLE_ID!;
+
   const tablesDB = new TablesDB(client);
   const users = new Users(client);
+
+  async function createRoomMessage() {
+    const room = await tablesDB.getRow({
+      databaseId: CRIC_TALK_DATABASE_ID,
+      tableId: ROOMS_TABLE_ID,
+      rowId: roomId,
+    });
+
+    if (room.status !== "live") {
+      return res.json({ error: "Unauthorized" }, 401);
+    }
+
+    await tablesDB.createRow({
+      databaseId: CRIC_TALK_DATABASE_ID,
+      tableId: ROOM_MESSAGE_TABLE_ID,
+      rowId: ID.unique(),
+      data: {
+        roomId,
+        content,
+        isEdited: false,
+      },
+    });
+  }
+
+  async function updateRoomMessage(roomMessageId: string) {
+    const room = await tablesDB.getRow({
+      databaseId: CRIC_TALK_DATABASE_ID,
+      tableId: ROOMS_TABLE_ID,
+      rowId: roomId,
+    });
+
+    if (room.status !== "live") {
+      return res.json({ error: "Unauthorized" }, 401);
+    }
+
+    await tablesDB.updateRow({
+      databaseId: CRIC_TALK_DATABASE_ID,
+      tableId: ROOM_MESSAGE_TABLE_ID,
+      rowId: roomMessageId,
+      data: {
+        content,
+      },
+    });
+  }
+
+  async function deleteRoomMessage(roomMessageId: string) {
+    const room = await tablesDB.getRow({
+      databaseId: CRIC_TALK_DATABASE_ID,
+      tableId: ROOMS_TABLE_ID,
+      rowId: roomId,
+    });
+
+    if (room.status !== "live") {
+      return res.json({ error: "Unauthorized" }, 401);
+    }
+
+    await tablesDB.deleteRow({
+      databaseId: CRIC_TALK_DATABASE_ID,
+      tableId: ROOM_MESSAGE_TABLE_ID,
+      rowId: roomMessageId,
+    });
+  }
 };
