1e83342d5c6ed3de41d9c915a90e495e43ccae1b
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-08 00:17:49 +0530

    Rename index.ts to main.js and move into /src in room-message-guard function

--- functions/room-message-guard/src/main.js
+++ functions/room-message-guard/src/main.js
@@ -1,35 +1,110 @@
-import { Client, Users } from 'node-appwrite';
+import { Client, ID, TablesDB, Users } from "node-appwrite";
 
-// This Appwrite function will be executed every time your function is triggered
-export default async ({ req, res, log, error }) => {
-  // You can use the Appwrite SDK to interact with other services
-  // For this example, we're using the Users service
-  const client = new Client()
-    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(req.headers['x-appwrite-key'] ?? '');
-  const users = new Users(client);
-
+export default async ({ req, res }) => {
   try {
-    const response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
-  } catch(err) {
-    error("Could not list users: " + err.message);
-  }
+    const userId = req.headers["x-appwrite-user-id"];
 
-  // The req object contains the request data
-  if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return res.text("Pong");
-  }
+    if (!userId) {
+      return res.json({ error: "Unauthorized" }, 401);
+    }
 
-  return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
-  });
+    const { roomId, content, roomMessageId, action } = req.bodyJson;
+
+    const client = new Client()
+      .setEndpoint(process.env.APPWRITE_ENDPOINT)
+      .setProject(process.env.APPWRITE_PROJECT_ID)
+      .setKey(process.env.APPWRIT_API_KEY);
+
+    const CRIC_TALK_DATABASE_ID = process.env.CRIC_TALK_DATABASE_ID;
+    const ROOMS_TABLE_ID = process.env.ROOMS_TABLE_ID;
+    const ROOM_MESSAGE_TABLE_ID = process.env.ROOM_MESSAGE_TABLE_ID;
+
+    const tablesDB = new TablesDB(client);
+    const users = new Users(client);
+
+    const user = await users.get(userId);
+    const username = user.name || user.email.split("@")[0];
+
+    async function createRoomMessage() {
+      const room = await tablesDB.getRow({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: ROOMS_TABLE_ID,
+        rowId: roomId,
+      });
+
+      if (room.status !== "live") {
+        return res.json({ error: "Unauthorized" }, 401);
+      }
+
+      await tablesDB.createRow({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: ROOM_MESSAGE_TABLE_ID,
+        rowId: ID.unique(),
+        data: {
+          roomId,
+          authorId: userId,
+          authorName: username,
+          content,
+          isEdited: false,
+        },
+      });
+    }
+
+    async function updateRoomMessage() {
+      const room = await tablesDB.getRow({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: ROOMS_TABLE_ID,
+        rowId: roomId,
+      });
+
+      if (room.status !== "live") {
+        return res.json({ error: "Unauthorized" }, 401);
+      }
+
+      await tablesDB.updateRow({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: ROOM_MESSAGE_TABLE_ID,
+        rowId: roomMessageId,
+        data: {
+          content,
+        },
+      });
+    }
+
+    async function deleteRoomMessage() {
+      const room = await tablesDB.getRow({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: ROOMS_TABLE_ID,
+        rowId: roomId,
+      });
+
+      if (room.status !== "live") {
+        return res.json({ error: "Unauthorized" }, 401);
+      }
+
+      await tablesDB.deleteRow({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: ROOM_MESSAGE_TABLE_ID,
+        rowId: roomMessageId,
+      });
+    }
+
+    switch (action) {
+      case "create":
+        await createRoomMessage();
+        break;
+      case "update":
+        await updateRoomMessage();
+        break;
+      case "delete":
+        await deleteRoomMessage();
+        break;
+      default:
+        return res.json({ error: "Invalid action" }, 400);
+    }
+
+    return res.json({ success: true });
+  } catch (error) {
+    return res.json({ error: error.message }, 500);
+  }
 };
