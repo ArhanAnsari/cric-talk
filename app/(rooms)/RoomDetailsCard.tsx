5d77febb8b06d0e95f9e9de55c0ce3792c8ee5a8
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-08 09:17:48 +0530

    Add icons for room details in RoomDetailsCard component

--- app/(rooms)/RoomDetailsCard.tsx
+++ app/(rooms)/RoomDetailsCard.tsx
@@ -37,12 +37,14 @@ const RoomDetailsCard = ({ roomId, room, isVisible, onClose }: Props) => {
 
             {/* MATCH INFO */}
             <View className="mt-6 gap-2">
-              <View className="flex-row">
+              <View className="flex-row items-center gap-2">
+                <Ionicons name="pricetag-outline" size={14} color="#45556c" />
                 <Text className="text-slate-500">Room ID</Text>
                 <Text className="ml-auto text-slate-900 text-sm">{roomId}</Text>
               </View>
 
-              <View className="flex-row items-center">
+              <View className="flex-row items-center gap-2">
+                <Ionicons name="trophy-outline" size={14} color="#45556c" />
                 <Text className="text-slate-500">Match Type</Text>
                 <View className="ml-auto bg-orange-500/10 px-2 py-0.5 rounded-full">
                   <Text className="text-orange-500 font-semibold text-sm">
@@ -51,7 +53,8 @@ const RoomDetailsCard = ({ roomId, room, isVisible, onClose }: Props) => {
                 </View>
               </View>
 
-              <View className="flex-row">
+              <View className="flex-row items-center gap-2">
+                <Ionicons name="time-outline" size={14} color="#45556c" />
                 <Text className="text-slate-500">Start Time</Text>
                 <Text className="ml-auto text-slate-900 text-sm">
                   {new Date(room?.startTime || "").toLocaleString("en-IN", {
@@ -61,7 +64,8 @@ const RoomDetailsCard = ({ roomId, room, isVisible, onClose }: Props) => {
                 </Text>
               </View>
 
-              <View className="flex-row">
+              <View className="flex-row items-center gap-2">
+                <Ionicons name="stop-outline" size={14} color="#45556c" />
                 <Text className="text-slate-500">End Time</Text>
                 <Text className="ml-auto text-slate-900 text-sm">
                   {new Date(room?.endTime || "").toLocaleString("en-IN", {
