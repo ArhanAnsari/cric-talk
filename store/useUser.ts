115b9e2ad448262504c6c69baca8d5653537058f
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-10 02:24:04 +0530

    Add messageCount state to useUser store

--- store/useUser.ts
+++ store/useUser.ts
@@ -4,16 +4,21 @@ type UserType = {
   username: string | null;
   email: string | null;
   favTeam: string | null;
+  messageCount: number;
   setUsername: (username: string) => void;
   setEmail: (email: string) => void;
   setFavTeam: (favTeam: string) => void;
+  setMessageCount: (count: number) => void;
 };
 
 export const useUser = create<UserType>((set) => ({
   username: null,
   favTeam: null,
   email: null,
+  messageCount: 0,
   setUsername: (username) => set({ username }),
   setEmail: (email) => set({ email }),
   setFavTeam: (favTeam) => set({ favTeam }),
+  setMessageCount: (count) =>
+    set((s) => ({ messageCount: s.messageCount + count })),
 }));
