720f67a6d5ec5ad07aa8b232cd1671d22d9d19d5
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-13 05:12:24 +0530

    Add header in Account screen

--- app/(profile)/Account.tsx
+++ app/(profile)/Account.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import React from "react";
+import { Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Account = () => {
   return (
-    <View>
-      <Text>Account</Text>
-    </View>
-  )
-}
+    <View className="flex-1 bg-white">
+      <View className="w-full h-30 bg-orange-500">
+        <SafeAreaView>
+          <View className="px-6 py-4 flex-row items-center">
+            <Pressable
+              className="w-10 h-10 bg-orange-600 items-center justify-center rounded-full transition-all duration-300 ease-in-out active:scale-[0.98] active:opacity-85"
+              onPress={() => router.back()}
+            >
+              <Ionicons name="arrow-back" size={18} color="white" />
+            </Pressable>
 
-export default Account
+            <View className="absolute left-0 right-0 items-center">
+              <Text className="text-white text-lg font-semibold">Account</Text>
+            </View>
+          </View>
+        </SafeAreaView>
+      </View>
+    </View>
+  );
+};
+
+export default Account;
