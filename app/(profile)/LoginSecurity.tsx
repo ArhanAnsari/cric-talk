98675e10bdde84468aadc1506dbf55f0ece095bd
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-13 11:15:26 +0530

    Add header in LoginSecurity screen

--- app/(profile)/LoginSecurity.tsx
+++ app/(profile)/LoginSecurity.tsx
@@ -1,10 +1,31 @@
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const LoginSecurity = () => {
   return (
-    <View>
-      <Text>LoginSecurity</Text>
+    <View className="flex-1 bg-white">
+      <View className="w-full h-30 bg-orange-500">
+        <SafeAreaView>
+          {/* HEADER */}
+          <View className="px-6 py-4 flex-row items-center">
+            <Pressable
+              className="w-10 h-10 bg-orange-600 items-center justify-center rounded-full transition-all duration-300 ease-in-out active:scale-[0.98] active:opacity-85"
+              onPress={() => router.back()}
+            >
+              <Ionicons name="arrow-back" size={18} color="white" />
+            </Pressable>
+
+            <View className="absolute left-0 right-0 items-center">
+              <Text className="text-white text-lg font-semibold">
+                Login and Security
+              </Text>
+            </View>
+          </View>
+        </SafeAreaView>
+      </View>
     </View>
   );
 };
