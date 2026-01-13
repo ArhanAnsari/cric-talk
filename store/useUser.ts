61e8b0feac6c64cae2617712a56415e8d79881c2
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-14 04:30:40 +0530

    Add to store user's joinDate in useUser store

--- store/useUser.ts
+++ store/useUser.ts
@@ -5,10 +5,12 @@ type UserType = {
   email: string | null;
   favTeam: string | null;
   messageCount: number;
+  joinDate: Date;
   setUsername: (username: string) => void;
   setEmail: (email: string) => void;
   setFavTeam: (favTeam: string) => void;
   setMessageCount: (count: number) => void;
+  setJoinDate: (date: Date) => void;
 };
 
 export const useUser = create<UserType>((set) => ({
@@ -16,9 +18,11 @@ export const useUser = create<UserType>((set) => ({
   favTeam: null,
   email: null,
   messageCount: 0,
+  joinDate: new Date(),
   setUsername: (username) => set({ username }),
   setEmail: (email) => set({ email }),
   setFavTeam: (favTeam) => set({ favTeam }),
   setMessageCount: (count) =>
     set((s) => ({ messageCount: s.messageCount + count })),
+  setJoinDate: (date) => set({ joinDate: date }),
 }));
