396ba8ae8b7a13d7acf83b9ccfd8fe2ddc649fb1
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-14 12:41:19 +0530

    Add create, update and delete post functions in posts-guard function

--- functions/posts-guard/src/main.js
+++ functions/posts-guard/src/main.js
@@ -1,4 +1,5 @@
 import { Client, TablesDB, Users } from "node-appwrite";
+import { ID } from "react-native-appwrite";
 
 export default async ({ req, res }) => {
   try {
@@ -18,8 +19,95 @@ export default async ({ req, res }) => {
     const tablesDB = new TablesDB(client);
     const users = new Users(client);
 
+    const CRIC_TALK_DATABASE_ID = process.env.APPWRITE_CRIC_TALK_DATABASE_ID;
+    const POSTS_TABLE_ID = process.env.APPWRITE_POSTS_TABLE_ID;
+
     const user = users.get(userId);
     const authorName = user.name || user.email.split("@")[0];
+
+    async function createPost() {
+      await tablesDB.createRow({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: POSTS_TABLE_ID,
+        rowId: ID.unique(),
+        data: {
+          content,
+          images,
+          authorId: userId,
+          authorName,
+          likes: 0,
+          likedBy: [],
+          views: 0,
+          viewedBy: [],
+          commentCount: 0,
+        },
+      });
+    }
+
+    async function updatePost() {
+      const post = await tablesDB.getRow({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: POSTS_TABLE_ID,
+        rowId: postId,
+      });
+
+      if (post.authorId !== userId) {
+        throw new Error("Forbidden: You aren't author of this post");
+      }
+
+      const likedBy = post.likedBy || [];
+      const viewedBy = post.viewedBy || [];
+
+      if (likedBy.includes(userId)) return;
+
+      await tablesDB.updateRow({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: POSTS_TABLE_ID,
+        rowId: postId,
+        data: {
+          content,
+          images,
+          likes,
+          likedBy: [...likedBy, userId],
+          views,
+          viewedBy: [...viewedBy, userId],
+        },
+      });
+    }
+
+    async function deletePost() {
+      const post = await tablesDB.getRow({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: POSTS_TABLE_ID,
+        rowId: postId,
+      });
+
+      if (post.authorId !== userId) {
+        throw new Error("Forbidden: You aren't author of this post");
+      }
+
+      await tablesDB.deleteRow({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: POSTS_TABLE_ID,
+        rowId: postId,
+      });
+    }
+
+    switch (action) {
+      case "create":
+        await createPost();
+        break;
+      case "update":
+        await updatePost();
+        break;
+      case "delete":
+        await deletePost();
+        break;
+      default:
+        throw new Error("Invalid action");
+    }
+
+    return res.json({ success: true });
   } catch (error) {
     throw new Error(`Unable to process query ${error}`);
   }
