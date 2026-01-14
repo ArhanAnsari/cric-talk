1d87d6cc64c0e293403094269b4c8495464bc2db
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-14 07:14:25 +0530

    Add createRoom and deleteRoom function in rooms-guard function

--- functions/rooms-guard/src/main.js
+++ functions/rooms-guard/src/main.js
@@ -1,4 +1,5 @@
 import { Client, TablesDB, Users } from "node-appwrite";
+import { ID } from "react-native-appwrite";
 
 export default async ({ req, res }) => {
   try {
@@ -27,8 +28,50 @@ export default async ({ req, res }) => {
     const tablesDB = new TablesDB(client);
     const users = new Users(client);
 
+    const CRIC_TALK_DATABASE_ID = process.env.APPWRITE_CRIC_TALK_DATABASE_ID;
+    const ROOMS_TABLE_ID = process.env.APPWRITE_ROOMS_TABLE_ID;
+
     const user = users.get(userId);
     const authorName = user.name || user.email.split("@")[0];
+
+    async function createRoom() {
+      const result = await tablesDB.createRow({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: ROOMS_TABLE_ID,
+        rowId: ID.unique(),
+        data: {
+          teams,
+          status,
+          authorId: userId,
+          authorName,
+          startTime,
+          endTime,
+          matchType,
+          isLocked,
+        },
+      });
+      return res.json(result);
+    }
+
+    async function deleteRoom() {
+      const result = await tablesDB.deleteRow({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: ROOMS_TABLE_ID,
+        rowId: roomId,
+      });
+      return res.json(result);
+    }
+
+    switch (action) {
+      case "create":
+        await createRoom();
+        break;
+      case "delete":
+        await deleteRoom();
+        break;
+      default: 
+        throw new Error('Invalid action')
+    }
   } catch (error) {
     throw new Error("Unable to process query");
   }
