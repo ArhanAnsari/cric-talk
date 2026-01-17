b037fefde5701a68663492d38c323b5eac24e36a
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-17 10:45:07 +0530

    Add old and new room states in RoomManage screen

--- app/(rooms)/RoomManage/[roomId].tsx
+++ app/(rooms)/RoomManage/[roomId].tsx
@@ -1,7 +1,7 @@
 import { useRooms } from "@/store/useRooms";
 import { Ionicons } from "@expo/vector-icons";
 import { router, useLocalSearchParams } from "expo-router";
-import React from "react";
+import React, { useState } from "react";
 import { Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -10,6 +10,23 @@ const RoomManage = () => {
 
   const rooms = useRooms((s) => s.rooms);
   const room = rooms.find((r) => r.$id === roomId);
+
+  const {
+    teams: oldTeams = [],
+    startTime: oldStartTime = "",
+    endTime: oldEndTime = "",
+    matchType: oldMatchType = "ODI",
+    isLocked: oldIsLocked = false,
+  } = room || {};
+
+  const [team1, setTeam1] = useState<string>(oldTeams[0]);
+  const [team2, setTeam2] = useState<string>(oldTeams[1]);
+  const [startTime, setStartTime] = useState<string>(oldStartTime);
+  const [endTime, setEndTime] = useState<string>(oldEndTime);
+  const [matchType, setMatchType] = useState<"ODI" | "TEST" | "T20">(
+    oldMatchType,
+  );
+  const [isLocked, setIsLocked] = useState<boolean>(oldIsLocked);
 
   return (
     <View className="flex-1 bg-white">
