47e9c4b78fe41e3d0b7240d3ec4a19f6f4a4147b
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-21 04:21:35 +0530

    Add header in NotificationsScreen

--- app/(notifications)/NotificationsScreen.tsx
+++ app/(notifications)/NotificationsScreen.tsx
@@ -1,10 +1,31 @@
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const NotificationsScreen = () => {
   return (
-    <View>
-      <Text>NotificationsScreen</Text>
+    <View className="flex-1 bg-white">
+      <View className="w-full h-30 bg-orange-500">
+        <SafeAreaView>
+          {/* HEADER */}
+          <View className="px-6 py-4 flex-row items-center">
+            <Pressable
+              className="size-10 bg-orange-600 items-center justify-center rounded-full transition-all duration-300 ease-in-out active:scale-[0.98] active:opacity-85"
+              onPress={() => router.back()}
+            >
+              <Ionicons name="arrow-back" size={18} color="white" />
+            </Pressable>
+
+            <View className="absolute left-0 right-0 items-center">
+              <Text className="text-lg text-white font-semibold">
+                Notifications
+              </Text>
+            </View>
+          </View>
+        </SafeAreaView>
+      </View>
     </View>
   );
 };
