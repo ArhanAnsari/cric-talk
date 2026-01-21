84f0002ffda1bcd410ad2abe90e6b8ea8d96732c
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-21 16:10:15 +0530

    Add fetchNotificationsByUserId function and action based function calling in notifications-guard function

--- functions/notifications-guard/src/main.js
+++ functions/notifications-guard/src/main.js
@@ -18,4 +18,24 @@ export default async ({ req, res }) => {
 
   const CRIC_TALK_DATABASE_ID = process.env.CRIC_TALK_DATABASE_ID;
   const NOTIFICATIONS_TABLE_ID = process.env.NOTIFICATIONS_TABLE_ID;
+
+  async function fetchNotificationsByUserId() {
+    return await tablesDB.listRows({
+      databaseId: CRIC_TALK_DATABASE_ID,
+      tableId: NOTIFICATIONS_TABLE_ID,
+      queries: [Query.equal('userId', userId), Query.orderDesc('$createdAt')],
+    });
+  }
+
+  let result;
+
+  switch (action) {
+    case 'fetchByUserId':
+      result = await fetchNotificationsByUserId();
+      break;
+    default:
+      throw new Error('Invalid action');
+  }
+
+  return res.json({ success: true, data: result });
 };
