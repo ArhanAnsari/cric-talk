b813c17281f3cc3fa5f88106aabba88dc5a10534
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-21 10:43:59 +0530

    Add sendPushToken and deletePushToken function to user-push-token-guard function

--- functions/user-push-token-guard/src/main.js
+++ functions/user-push-token-guard/src/main.js
@@ -1,4 +1,4 @@
-import { Client, TablesDB, Users } from 'node-appwrite';
+import { Client, Operator, TablesDB, Users } from 'node-appwrite';
 
 export default async ({ req, res }) => {
   const userId = req.headers['x-appwrite-user-id'];
@@ -19,4 +19,22 @@ export default async ({ req, res }) => {
 
   const CRIC_TALK_DATABASE_ID = process.env.APPWRITE_CRIC_TALK_DATABASE_ID;
   const USERS_TABLE_ID = process.env.APPWRITE_USERS_TABLE_ID;
+
+  async function sendPushToken() {
+    return await tablesDB.upsertRow({
+      databaseId: CRIC_TALK_DATABASE_ID,
+      tableId: USERS_TABLE_ID,
+      rowId: userId,
+      data: { pushTokens: Operator.arrayAppend(pushToken) },
+    });
+  }
+
+  async function deletePushToken() {
+    await tablesDB.updateRow({
+      databaseId: CRIC_TALK_DATABASE_ID,
+      tableId: USERS_TABLE_ID,
+      rowId: userId,
+      data: { pushTokens: Operator.arrayRemove(pushToken) },
+    });
+  }
 };
