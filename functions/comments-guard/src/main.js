e43efa6fa44138b2512e9c85ac91757e1e27af1d
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-23 04:52:53 +0530

    Add content and postId validation to addComment function in comments-guard function'

--- functions/comments-guard/src/main.js
+++ functions/comments-guard/src/main.js
@@ -20,6 +20,9 @@ export default async ({ req, res }) => {
     const COMMENTS_TABLE_ID = process.env.APPWRITE_COMMENTS_TABLE_ID;
 
     async function addComment() {
+      if (!content) throw new Error("Error: Comment can't be empty");
+      if (!postId) throw new Error('Error: Post ID is required to add comment');
+
       return await tablesDB.createRow({
         databaseId: CRIC_TALK_DATABASE_ID,
         tableId: COMMENTS_TABLE_ID,
