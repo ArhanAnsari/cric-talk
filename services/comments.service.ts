466a60e8b90f341c3ea1f55516d1c0a1a33d6d63
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-22 21:09:10 +0530

    Remove unused addComment and deleteComment service function in comments service

--- services/comments.service.ts
+++ services/comments.service.ts
@@ -1,6 +1,6 @@
 import { CommentType } from "@/interfaces/Post";
 import { functions, tablesDB } from "@/libs/appwrite";
-import { ID, Query } from "react-native-appwrite";
+import { Query } from "react-native-appwrite";
 
 const CRIC_TALK_DATABASE_ID =
   process.env.EXPO_PUBLIC_APPWRITE_CRIC_TALK_DATABASE_ID!;
@@ -17,42 +17,6 @@ export async function fetchComments(postId: string) {
     });
   } catch (error) {
     console.log(`Error while fetching the comments ${error}`);
-    throw error;
-  }
-}
-
-export async function addComment(
-  postId: string,
-  authorId: string,
-  content: string,
-) {
-  try {
-    return await tablesDB.createRow<CommentType>({
-      databaseId: CRIC_TALK_DATABASE_ID,
-      tableId: COMMENTS_TABLE_ID,
-      rowId: ID.unique(),
-      data: {
-        postId,
-        authorId,
-        content,
-        isEdited: false,
-      },
-    });
-  } catch (error) {
-    console.log(`Error while adding the comment ${error}`);
-    throw error;
-  }
-}
-
-export async function deleteComment(commentId: string) {
-  try {
-    return await tablesDB.deleteRow({
-      databaseId: CRIC_TALK_DATABASE_ID,
-      tableId: COMMENTS_TABLE_ID,
-      rowId: commentId,
-    });
-  } catch (error) {
-    console.log(`Error while deleting the comment ${error}`);
     throw error;
   }
 }
