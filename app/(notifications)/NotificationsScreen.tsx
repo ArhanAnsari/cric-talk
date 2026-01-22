0d0477a6d7a45de34f3fbe672e705e19ec3e9222
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-22 06:25:38 +0530

    Refactor notification card into NotificationCard component and use it in NotificationsScreen

--- app/(notifications)/NotificationsScreen.tsx
+++ app/(notifications)/NotificationsScreen.tsx
@@ -8,6 +8,7 @@ import { router } from "expo-router";
 import React, { useEffect, useState } from "react";
 import { FlatList, Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import NotificationCard from "../components/NotificationCard";
 
 const NotificationsScreen = () => {
   const [expoPushToken, setExpoPushToken] = useState<string>("");
@@ -93,41 +94,7 @@ const NotificationsScreen = () => {
           data={notificationsList}
           keyExtractor={(item) => item.$id}
           contentContainerStyle={{ paddingBottom: 80, marginTop: 16 }}
-          renderItem={({ item }) => (
-            <Pressable className="bg-white p-4 rounded-xl shadow-md elevation-xs border border-slate-300 mb-6 transition-all duration-300 ease-in-out active:scale-[0.95] active:opacity-85">
-              {/* TITLE */}
-              <View className="flex-row items-center gap-3">
-                <View className="bg-slate-100 size-10 items-center justify-center rounded-full">
-                  <Ionicons
-                    name="notifications-outline"
-                    size={18}
-                    color="#0f172b"
-                  />
-                </View>
-
-                <Text className="font-medium text-slate-900">{item.title}</Text>
-
-                <View className="ml-auto bg-slate-100 px-2 py-0.5 rounded-full">
-                  <Text className="text-sm text-slate-600 font-mediumW">
-                    {Math.floor(
-                      (Date.now() - new Date(item.$createdAt).getTime()) /
-                        1000 /
-                        60 /
-                        60,
-                    )}{" "}
-                    hr ago
-                  </Text>
-                </View>
-              </View>
-
-              {/* CONTENT */}
-              <View>
-                <Text className="mt-2 text-sm leading-6 text-slate-700">
-                  {item.content}
-                </Text>
-              </View>
-            </Pressable>
-          )}
+          renderItem={({ item }) => <NotificationCard notification={item} />}
         />
       </View>
     </View>
---
+++ app/components/NotificationCard.tsx
@@ -0,0 +1,45 @@
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { Pressable, Text, View } from "react-native";
+
+type Props = {
+  title: string;
+  content: string;
+  $createdAt: string;
+};
+
+const NotificationCard = ({ notification }: { notification: Props }) => {
+  return (
+    <Pressable className="bg-white p-4 rounded-xl shadow-md elevation-xs border border-slate-300 mb-6 transition-all duration-300 ease-in-out active:scale-[0.95] active:opacity-85">
+      {/* TITLE */}
+      <View className="flex-row items-center gap-3">
+        <View className="bg-slate-100 size-10 items-center justify-center rounded-full">
+          <Ionicons name="notifications-outline" size={18} color="#0f172b" />
+        </View>
+
+        <Text className="font-medium text-slate-900">{notification.title}</Text>
+
+        <View className="ml-auto bg-slate-100 px-2 py-0.5 rounded-full">
+          <Text className="text-sm text-slate-600 font-mediumW">
+            {Math.floor(
+              (Date.now() - new Date(notification.$createdAt).getTime()) /
+                1000 /
+                60 /
+                60,
+            )}{" "}
+            hr ago
+          </Text>
+        </View>
+      </View>
+
+      {/* CONTENT */}
+      <View>
+        <Text className="mt-2 text-sm leading-6 text-slate-700">
+          {notification.content}
+        </Text>
+      </View>
+    </Pressable>
+  );
+};
+
+export default NotificationCard;
