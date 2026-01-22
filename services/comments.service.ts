f030208d5a5086aca5d03dc132924011a0f1a79c
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-01-22 20:27:47 +0530

    Add executeComment function in comments service

--- services/comments.service.ts
+++ services/comments.service.ts
@@ -1,10 +1,12 @@
 import { CommentType } from "@/interfaces/Post";
-import { tablesDB } from "@/libs/appwrite";
+import { functions, tablesDB } from "@/libs/appwrite";
 import { ID, Query } from "react-native-appwrite";
 
 const CRIC_TALK_DATABASE_ID =
   process.env.EXPO_PUBLIC_APPWRITE_CRIC_TALK_DATABASE_ID!;
 const COMMENTS_TABLE_ID = process.env.EXPO_PUBLIC_APPWRITE_COMMENTS_TABLE_ID!;
+const COMMENTS_GUARD_FUNCTION_ID =
+  process.env.EXPO_PUBLIC_APPWRITE_COMMENTS_GUARD_FUNCTION_ID!;
 
 export async function fetchComments(postId: string) {
   try {
@@ -22,7 +24,7 @@ export async function fetchComments(postId: string) {
 export async function addComment(
   postId: string,
   authorId: string,
-  content: string
+  content: string,
 ) {
   try {
     return await tablesDB.createRow<CommentType>({
@@ -52,5 +54,27 @@ export async function deleteComment(commentId: string) {
   } catch (error) {
     console.log(`Error while deleting the comment ${error}`);
     throw error;
+  }
+}
+
+export async function executeComment({
+  action,
+  postId,
+  commentId,
+  content,
+}: {
+  action: "add" | "delete";
+  postId?: string;
+  commentId?: string;
+  content?: string;
+}) {
+  try {
+    return await functions.createExecution({
+      functionId: COMMENTS_GUARD_FUNCTION_ID,
+      body: JSON.stringify({ action, postId, commentId, content }),
+      async: false,
+    });
+  } catch (error) {
+    console.log(`Error while executing comment ${action} action`);
   }
 }
