e5a26be7f40487aecb463ed779f15a323063a647
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-02-02 05:06:09 +0530

    Add rateLimitCheck function in rooms-guard

--- functions/rooms-guard/src/main.js
+++ functions/rooms-guard/src/main.js
@@ -21,9 +21,62 @@ export default async ({ req, res }) => {
 
     const CRIC_TALK_DATABASE_ID = process.env.APPWRITE_CRIC_TALK_DATABASE_ID;
     const ROOMS_TABLE_ID = process.env.APPWRITE_ROOMS_TABLE_ID;
+    const RATE_LIMIT_TABLE_ID = process.env.APPWRITE_RATE_LIMIT_TABLE_ID;
 
     const user = await users.get(userId);
     const authorName = user.name || user.email.split('@')[0];
+
+    async function rateLimitCheck(activity) {
+      // implement rate limit
+      // 10 requests per minute per user
+
+      // get windowKey and userActivity
+      const windowKey = Math.floor(Date.now() / 60000);
+      const userActivity = await tablesDB.listRows({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: RATE_LIMIT_TABLE_ID,
+        queries: [
+          Query.equal('userId', userId),
+          Query.equal('activity', activity),
+          Query.equal('windowKey', windowKey),
+          Query.limit(1),
+        ],
+      });
+
+      // if userActivity doesn't exists create one
+      if (userActivity.rows.length === 0) {
+        await tablesDB.createRow({
+          databaseId: CRIC_TALK_DATABASE_ID,
+          tableId: RATE_LIMIT_TABLE_ID,
+          rowId: ID.unique(),
+          data: {
+            userId,
+            activity,
+            windowKey,
+            activityCount: 1,
+          },
+        });
+        return;
+      }
+
+      // check rate limit of user
+      const row = userActivity.rows[0];
+
+      if (row.activityCount >= 10) {
+        throw new Error(
+          'Rate limit exceeded: Too many requests in a short period'
+        );
+      }
+
+      // increase activityCount if not rate limit exceeded
+      await tablesDB.incrementRowColumn({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: RATE_LIMIT_TABLE_ID,
+        rowId: row.$id,
+        column: 'activityCount',
+        value: 1,
+      });
+    }
 
     async function createRoom() {
       let status;
