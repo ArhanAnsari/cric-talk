a0d037832fe3db81d739dfe712bda262466da0c9
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-02-01 08:14:59 +0530

    Add rateLimitCheck function in room-message-guard

--- functions/room-message-guard/src/main.js
+++ functions/room-message-guard/src/main.js
@@ -21,11 +21,64 @@ export default async ({ req, res }) => {
     const USERS_TABLE_ID = process.env.USERS_TABLE_ID;
     const NOTIFICATIONS_TABLE_ID = process.env.APPWRITE_NOTIFICATIONS_TABLE_ID;
 
+    const RATE_LIMIT_TABLE_ID = process.env.APPWRITE_RATE_LIMIT_TABLE_ID;
+
     const tablesDB = new TablesDB(client);
     const users = new Users(client);
 
     const user = await users.get(userId);
     const username = user.name || user.email.split('@')[0];
+
+    async function rateLimitCheck(activity) {
+      // implement rate limit
+      // 60 requests per minute per user
+
+      // get windowKey and userActivity
+      const windowKey = Math.floor(Date.now() / 60000);
+      const userActivity = await tablesDB.listRows({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: RATE_LIMIT_TABLE_ID,
+        queries: [
+          Query.equal('userId', userId),
+          Query.equal('activity', activity),
+          Query.equal('windowKey', windowKey),
+        ],
+      });
+
+      // if userActiity doesn't exist create one
+      if (userActivity.rows.length === 0) {
+        await tablesDB.createRow({
+          databaseId: CRIC_TALK_DATABASE_ID,
+          tableId: RATE_LIMIT_TABLE_ID,
+          rowId: ID.unique(),
+          data: {
+            userId,
+            activity,
+            windowKey,
+            activityCount: 60,
+          },
+        });
+        return;
+      }
+
+      // check activityCount
+      const row = activity.rows[0];
+
+      if (row.activityCount >= 60) {
+        throw new Error(
+          'Rate limit exceeded: Too many requests in a short period'
+        );
+      }
+
+      // increase if limit not exceeded
+      await tablesDB.incrementRowColumn({
+        databaseId: CRIC_TALK_DATABASE_ID,
+        tableId: RATE_LIMIT_TABLE_ID,
+        rowId: row.$id,
+        column: activityCount,
+        value: 1,
+      });
+    }
 
     async function createRoomMessage() {
       const room = await tablesDB.getRow({
