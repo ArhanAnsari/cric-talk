919cac77ea8dadff7500f58518904e94358eec4f
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-02-02 06:55:37 +0530

    Fix Query import in posts-guard from node-appwrite instead of react-native-appwrite

--- functions/posts-guard/src/main.js
+++ functions/posts-guard/src/main.js
@@ -1,5 +1,4 @@
-import { Client, ID, TablesDB, Users } from 'node-appwrite';
-import { Query } from 'react-native-appwrite';
+import { Client, ID, TablesDB, Users, Query } from 'node-appwrite';
 
 export default async ({ req, res }) => {
   try {
