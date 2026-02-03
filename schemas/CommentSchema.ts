c97fcfe0e0753a8235819d479eb4194560bf5fac
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2026-02-03 18:37:56 +0530

    Add CommentSchema using zod for validating comment data

---
+++ schemas/CommentSchema.ts
@@ -0,0 +1,20 @@
+import { Models } from "react-native-appwrite";
+import * as z from "zod";
+
+export const BaseCommentSchema = z.object({
+  postId: z.string().min(1, "Invalid post"),
+  content: z
+    .string()
+    .trim()
+    .min(1, "Comment can not be empty")
+    .max(500, "Comment can not be more than 500 characters"),
+});
+
+export const CreateCommentSchema = BaseCommentSchema;
+
+export const CommentSchema = BaseCommentSchema.extend({
+  authorId: z.string(),
+  isEdited: z.boolean(),
+});
+
+export type CommentType = Models.Row & z.infer<typeof CommentSchema>;
